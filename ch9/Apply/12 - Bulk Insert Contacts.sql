ecc8203ded8def6e4307e2f60b997b769a343668
Frank Ibem <soulsolja@yahoo.com>
2017-06-08 22:04:55 -0500

    Added ch9

---
+++ ch9/Apply/12 - Bulk Insert Contacts.sql
@@ -0,0 +1,13 @@
+USE AddressBook;
+
+BULK INSERT dbo.Contacts FROM 'C:\temp\sqlbasics\importfiles\01_contacts.csv'
+	WITH (ROWTERMINATOR = '\n', FIELDTERMINATOR = ',', FIRSTROW = 2,
+		ERRORFILE = 'c:\temp\sqlbasics\importfiles\01_Contact_Errors.csv',
+		CHECK_CONSTRAINTS);
+
+--BULK INSERT dbo.Contacts FROM 'C:\temp\sqlbasics\importfiles\01_contacts.csv'
+--	WITH (ROWTERMINATOR = '\n', FIELDTERMINATOR = ',', FIRSTROW = 2,
+--		ERRORFILE = 'c:\temp\sqlbasics\importfiles\01_Contact_Errors.csv');
+
+
+GO
